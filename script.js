e3a1786eec11d9f4237aab2bdf051d396c48c655
rosary-hub <alenanto237@gmail.com>
2024-07-03 12:36:18 +0530

    Update script.js
    Added Counters funtionality and windows onload feature for displaying the count from the beginning in counters.

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function calculateBalance() {
     const debit = parseFloat(document.getElementById('debit').value) || 0;
 
     const currentBalance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 0;
+    const total = localStorage.getItem('totalRecited') ? parseFloat(localStorage.getItem('totalRecited')) : 0;
+
     let newBalance = currentBalance + credit - debit;
+    let totalCount = total+credit;
 
     localStorage.setItem('balance', newBalance);
+    localStorage.setItem('totalRecited', totalCount);
 
-    document.getElementById('balance').textContent = `Your current balance is: ${newBalance}`;
+    document.getElementById('balance').textContent = `${newBalance}`;
+    document.getElementById('totalRecited').textContent = `${totalCount}`;
     document.getElementById('credit').value='';
     document.getElementById('debit').value='';
 }
+window.onload=calculateBalance;
